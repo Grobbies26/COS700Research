aca3dd71a1b783967e76368d22b10e148c79e21b
Gabriel Grobler <grobler.gabriel12@gmail.com>
2023-10-09 22:27:14 +0200

    Changed to Use of Merkle Tree

--- main.py
+++ main.py
@@ -1,112 +1,149 @@
+# Gabriel Grobler Research Project
 
-import PyPDF4 # useful
 import pdfrw # useful
-import hashlib
-# import pdfminer # might be useful
-# import reportlab
-# import pypdftk
+from merkly.mtree import MerkleTree
 
-# from io import StringIO
-# from pdfminer.converter import TextConverter
-# from pdfminer.layout import LAParams
-# from pdfminer.pdfdocument import PDFDocument
-# from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-# from pdfminer.pdfpage import PDFPage
-# from pdfminer.pdfparser import PDFParser
 
-def pypdftest(pdf_path):
-    with open(pdf_path, 'rb') as f:
-        pdf = PyPDF4.PdfFileReader(f)
+#Calculate the Hash Values for the PDF
+def calculateHashValues(filePageObjects = []):
+    if filePageObjects == []:
+        return
 
-        print(pdf.xref) # xref table
-        print()
-        pages = list(pdf.pages)
-        print(pages)
-        print()
-        print(pages[0].extractText())
-        print()
-        print(pages[1])
-        print()
-        # print(pdf.resolvedObjects) # gets the page objects
-        # print() 
-        # print(pdf.resolvedObjects[(0,1)]) # gets the page objects
-        # print() 
-        # print(pdf.resolvedObjects[(0,2)])
-        # print() 
-        # print(pdf.resolvedObjects[(0,3)])
-        # print()
-        # print(pdf.resolvedObjects[(0,14)])
-        # print()
-        # print(pdf.getObject((0,1))) # work out how ro do this
+    content = []
+    hashes = []
+    w = 256
 
-        # information = pdf.getDocumentInfo()
-        # number_of_pages = pdf.getNumPages()
+    for i, page in enumerate(filePageObjects):  
+        content = [page.Contents.stream[i:i + w] for i in range(0, len(page.Contents.stream), w)]
 
-    return
+        tree = MerkleTree(content)
 
-def pdfrwtest(pdf_path):
-    pdf = pdfrw.PdfReader(pdf_path)
+        hash = {"root": tree.root, "leafs": tree.leafs}
+        hashes.append(hash)
+
+    return hashes
+
+#Retrieve the previously stored Hash Values
+def getHashValues(filePageObjects = []):
+    if filePageObjects == {}:
+        return
     
-    print(pdf.keys())
-    print()
-    print(pdf.Info)
-    print()
-    print(pdf.Root.keys())
-    print()
-    pages = pdf.pages
-    print(pages)
-    print()
-    print(pages[0])
-    print()
-    print(pages[0].Contents)
-    print()
-    print(pages[0].Contents.stream)
-    print()
-    hash = hashlib.sha256(pages[0].Contents.stream.encode('utf-8')).hexdigest()
-    print(hash)    
-    print()
-    print(pages[1])
-    print()
-    print(pages[1].Contents.stream)
-    print()
+    hashes = []
+    for i, page in enumerate(filePageObjects):
+        root = page.hashroot.replace("(","").replace(")","")
+        leafs = [val.replace("(","").replace(")","") for val in page.hashleafs]
+        hashes.append({"root":root,"leafs":leafs})
 
-    # opages = []
-    # opages.append(pages.pop(0))
-    # pdfrw.PdfWriter('./Test_PDFS/spamPDF.pdf').addpages(opages).write()
+    return hashes
+
+#Get the file page objects for the PDF
+def getFilePageObjects(pdf_path = ""):
+    if pdf_path == "":
+        return
+    
+    pdf = pdfrw.PdfReader(pdf_path)
+    filePageObjects = pdf.pages
+
+    return filePageObjects
+
+#Insert the Hash Values into the PDF
+def insertHashValues(writer, pages=[], hashes = []):
+    if hashes == [] or pages == []:
+        return
+    
+    for i, page in enumerate(pages):
+        page.hashroot = hashes[i]["root"]
+        page.hashleafs = hashes[i]["leafs"]
+
+        print("Page: ", page,"\n")
+
+    writer.addpages(pages)
+
+    return writer
+
+#Save the PDf 
+def savePDF(writer):
+    writer.write()
 
     return
 
-# def pdfminertest(pdf_path):
-#     with open(pdf_path, 'rb') as f:
-#         output_string = StringIO()
-#         parser = PDFParser(f)
-#         doc = PDFDocument(parser)
-#         rsrcmgr = PDFResourceManager()
-#         device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
-#         interpreter = PDFPageInterpreter(rsrcmgr, device)
-#         for page in PDFPage.create_pages(doc):
-#             interpreter.process_page(page)
-#             print(page)
-        
-#     return
+# Get all the data within the file
+def getAllData(path = "", saveName = ""):
+    if path == "":
+        return
+    
+    pdf = pdfrw.PdfReader(path)
+    keys = pdf.keys()
+    info = pdf.Info
+    rootKeys = pdf.Root.keys()
+    # print("Keys: ",keys,"\n")
+    # print("Info: ",info,"\n")
+    # print("Root Keys: ",rootKeys,"\n")
 
-# def reportlabtest(pdf_path):
-#     with open(pdf_path, 'rb') as f:
-#         output_string = StringIO()
-#         parser = PDFParser(f)
-#         doc = PDFDocument(parser)
-#         rsrcmgr = PDFResourceManager()
-#         device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
-#         interpreter = PDFPageInterpreter(rsrcmgr, device)
-#         for page in PDFPage.create_pages(doc):
-#             interpreter.process_page(page)
-#             print(page)
-        
-#     return
+    filePageObjects = getFilePageObjects(path)
+    hashes = calculateHashValues(filePageObjects)
 
+    if saveName == "":
+        saveName = path.replace('.pdf',"_hash.pdf") 
+
+    writer = pdfrw.PdfWriter(saveName)
+    writer = insertHashValues(writer, filePageObjects, hashes)
+    savePDF(writer)
+
+
+    filePageObjects2 = getFilePageObjects(saveName)
+    hashes2 = calculateHashValues(filePageObjects2)
+    hashes3 = getHashValues(filePageObjects2)
+    print(hashes2)
+    print(hashes3)
+
+
+# Protect the PDF against tampering
+def protectPDF(path = "", saveName = ""):
+    if path == "":
+        print("Invalid Path Provided")
+        return
+    
+    pdf = pdfrw.PdfReader(path)
+    keys = pdf.keys()
+    info = pdf.Info
+    rootKeys = pdf.Root.keys()
+    # print("Keys: ",keys,"\n")
+    # print("Info: ",info,"\n")
+    # print("Root Keys: ",rootKeys,"\n")
+
+    filePageObjects = getFilePageObjects(path)
+    hashes = calculateHashValues(filePageObjects)
+
+    if saveName == "":
+        saveName = path.replace('.pdf',"_hash.pdf") 
+
+    writer = pdfrw.PdfWriter(saveName)
+    writer = insertHashValues(writer, filePageObjects, hashes)
+    savePDF(writer)
+
+# Assess if the PDF has been tampered with
+def assessForForgery(path = ""):
+    if path == "":
+        print("Invalid Path Provided")
+        return
+    
+    filePageObjects2 = getFilePageObjects(path)
+    hashes2 = calculateHashValues(filePageObjects2)
+    hashes3 = getHashValues(filePageObjects2)
+    print(hashes2)
+    print(hashes3)
+    print(hashes2 == hashes3)
+
+
+#Main function
 if __name__ == '__main__':
-    # path = './Test_PDFS/test.pdf'
     path = './Test_PDFS/simplePDF.pdf'
-    # pypdftest(path) # Gives page objects investigate further
-    pdfrwtest(path) # Gives page objects investigate further
+    pathForg = './Test_PDFS/simplePDF_hash.pdf'
+
+    # getAllData(path)
+
+    protectPDF(path)
+
+    assessForForgery(pathForg)
     
